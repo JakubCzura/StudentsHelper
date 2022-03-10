386dfc33885cca0ad8748c7f9b7250c362589d79
JakubCzura <76125047+JakubCzura@users.noreply.github.com>
2022-03-11 00:44:34 +0100

    Dodaj pliki projektów.

---
+++ StudentsHelper/Themes/EnumThemes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentsHelper.Themes
+{
+    public class EnumThemes
+    {
+        public enum Themes
+        {
+            Standard = 0,
+            Green = 1,
+            Pink = 2,
+            Blue = 3,
+            Yellow = 4
+        }
+    }
+}
---
+++ StudentsHelper/ViewModel/StudentsHelperVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentsHelper.ViewModel
+{
+    public class StudentsHelperVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
