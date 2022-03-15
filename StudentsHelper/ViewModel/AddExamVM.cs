4cd1bdc8bb8ac7f5fba8bef9c1f6cec2a6ed3f3c
JakubCzura <76125047+JakubCzura@users.noreply.github.com>
2022-03-15 19:02:22 +0100

    dodać zamkniecie okna z egzaminami

--- StudentsHelper/ViewModel/AddExamVM.cs
+++ StudentsHelper/ViewModel/AddExamVM.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StudentsHelper.ViewModel.Commands;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,12 @@ namespace StudentsHelper.ViewModel
 {
     internal class AddExamVM
     {
+        public AddExamVM()
+        {
+            CloseWindowCommand = new CloseWindowCommand(this);
+        }
+
+        public CloseWindowCommand CloseWindowCommand { get; set; }
+
     }
 }
---
+++ StudentsHelper/ViewModel/Commands/CloseWindowCommand.cs
@@ -0,0 +1,39 @@
+﻿using StudentsHelper.View.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace StudentsHelper.ViewModel.Commands
+{
+    internal class CloseWindowCommand : ICommand
+    {
+        public CloseWindowCommand(AddExamVM addExamVM /*AddExamWindow addExamWindow*/)
+        {
+            AddExamVM = addExamVM;
+            //AddExamWindow = addExamWindow;
+        }
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        AddExamVM AddExamVM { get; set; }
+        //AddExamWindow AddExamWindow { get; set; }
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+           //AddExamWindow.Close();
+        }
+    }
+}
