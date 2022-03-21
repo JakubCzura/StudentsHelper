f89138925aadc0569e6634433d2ca0fc545b392c
JakubCzura <76125047+JakubCzura@users.noreply.github.com>
2022-03-21 19:33:33 +0100

    ok

---
+++ StudentsHelper/Model/IUser.cs
@@ -0,0 +1,10 @@
+﻿namespace StudentsHelper.Model
+{
+    internal interface IUser
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+        string SecondName { get; set; }
+        int Age { get; set; }
+    }
+}
---
+++ StudentsHelper/Model/Student.cs
@@ -0,0 +1,42 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentsHelper.Model
+{
+    internal class Student : IUser
+    {
+        
+        private int id = 0;
+        [PrimaryKey, AutoIncrement]
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        private string name = string.Empty;
+        public string Name 
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private string secondName = string.Empty;
+        public string SecondName 
+        {
+            get { return secondName; }
+            set { secondName = value; }
+        }
+
+        private int age = 0;
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+    }
+}
