894715b6c124a533b7573f38718c82c6d9a8013e
Jakub Czura <czurajakub@gmail.com>
2022-08-31 16:26:17 +0200

    ok

---
+++ StudentsHelper/DataValidators/HomeworkDataValidator.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace StudentsHelper.DataValidators
+{
+    public static class HomeworkDataValidator
+    {
+        public static bool ValidateLessonName(string lessonName)
+        {
+            if (string.IsNullOrWhiteSpace(lessonName))
+            {
+                throw new ArgumentException("Brak danych o nazwie przedmiotu ", nameof(lessonName));
+            }
+            if (!(lessonName.Length <= 100 && lessonName.Length > 0))
+            {
+                throw new ArgumentException("Długość nazwy przedmiotu spoza zakresu znaków 1-100 ", nameof(lessonName));
+            }
+            if (!lessonName.All(char.IsLetterOrDigit))
+            {
+                throw new ArgumentException("Nazwa przedmiotu zawiera inne znaki niż litery i cyfry ", nameof(lessonName));
+            }
+            return true;
+        }
+
+        public static bool ValidateDateOfEnd(DateTime dateOfEnd)
+        {
+            if (!(dateOfEnd.Year >= DateTime.MinValue.Year && dateOfEnd.Year <= DateTime.MaxValue.Year))
+            {
+                throw new ArgumentException("Niepoprawna wartość roku ", nameof(dateOfEnd));
+            }
+            if (!(dateOfEnd.Month >= 1 && dateOfEnd.Month <= 12))
+            {
+                throw new ArgumentException("Niepoprawna wartość miesiąca ", nameof(dateOfEnd));
+            }
+            if (!(dateOfEnd.Day >= 1 && dateOfEnd.Day <= DateTime.DaysInMonth(dateOfEnd.Year, dateOfEnd.Month)))
+            {
+                throw new ArgumentException("Niepoprawna wartość dnia ", nameof(dateOfEnd));
+            }
+            return true;
+        }
+
+        public static bool ValidateExercise(string exercise)
+        {
+            if (string.IsNullOrWhiteSpace(exercise))
+            {
+                throw new ArgumentException("Brak danych o zadaniu domowym ", nameof(exercise));
+            }
+            if (!(exercise.Length <= 100 && exercise.Length > 0))
+            {
+                throw new ArgumentException("Długość zadania domowego spoza zakresu znaków 1-100 ", nameof(exercise));
+            }
+            return true;
+        }
+
+        public static bool ValidateNote(string note)
+        {
+            if (string.IsNullOrWhiteSpace(note))
+            {
+                throw new ArgumentException("Brak danych o notatce do zadania domowego ", nameof(note));
+            }
+            if (!(note.Length <= 500 && note.Length > 0))
+            {
+                throw new ArgumentException("Długość notatki do zadania domowego spoza zakresu znaków 1-500 ", nameof(note));
+            }
+            return true;
+        }
+    }
+}
---
+++ StudentsHelper/DataValidators/UserDataValidator.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using System.Xml.Linq;
+
+namespace StudentsHelper.DataValidators
+{
+    public static class UserDataValidator
+    {
+        public static bool ValidateName(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("Brak danych o imieniu ", nameof(name));
+            }
+            if (!(name.Length <= 100 && name.Length > 0))
+            {
+                throw new ArgumentException("Długość imienia spoza zakresu znaków 1-100 ", nameof(name));
+            }
+            if (!name.All(char.IsLetter))
+            {
+                throw new ArgumentException("Imie zawiera inne znaki niż litery ", nameof(name));
+            }
+            return true;
+        }
+
+        public static bool ValidateSecondName(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("Brak danych o nazwisku ", nameof(name));
+            }
+            if (!(name.Length <= 100 && name.Length > 0))
+            {
+                throw new ArgumentException("Długość nazwiska spoza zakresu znaków 1-100 ", nameof(name));
+            }
+            if (!name.All(char.IsLetter))
+            {
+                throw new ArgumentException("Nazwisko zawiera inne znaki niż litery ", nameof(name));
+            }
+            return true;
+        }
+
+        public static bool ValidateAge(int age)
+        {
+            if (!(age > 0 && age < 100))
+            {
+                throw new ArgumentException("Wiek spoza zakresu 0-100 ", nameof(age));
+            }
+            return true;
+        }
+
+        public static bool ValidateLogin(string login)
+        {
+            if (string.IsNullOrWhiteSpace(login))
+            {
+                throw new ArgumentException("Brak danych o loginie ", nameof(login));
+            }
+            if (login.Length != 7)
+            {
+                throw new ArgumentException("Długość loginu jest inna niż 7, uwzględaniając literę 's' ", nameof(login));
+            }
+            if (!login.All(char.IsLetterOrDigit))
+            {
+                throw new ArgumentException("Login zawiera inne znaki niż litery i cyfry ", nameof(login));
+            }
+            if (!login.StartsWith('s'))
+            {
+                throw new ArgumentException("Login musi zaczynać się od litery 's' i zawierać 6 cyfr", nameof(login));
+            }
+            if (!login.Substring(1).All(char.IsDigit))
+            {
+                throw new ArgumentException("Login musi zaczynać się od litery 's' i zawierać 6 cyfr ", nameof(login));
+            }
+            return true;
+        }
+
+        public static bool ValidatePassword(string password)
+        {
+            if (string.IsNullOrWhiteSpace(password))
+            {
+                throw new ArgumentException("Brak danych o haśle ", nameof(password));
+            }
+            if (!(password.Length >= 5 && password.Length <= 30))
+            {
+                throw new ArgumentException("Długość hasła jest spoza zakresu 5-30 ", nameof(password));
+            }
+            if (!password.All(char.IsAscii))
+            {
+                throw new ArgumentException("Hasło musi składać się ze znaków ASCII ", nameof(password));
+            }
+            if (!password.Any(char.IsDigit))
+            {
+                throw new ArgumentException("Hasło musi zawierać conajmniej jedną cyfrę", nameof(password));
+            }
+            if (!password.Any(char.IsLower))
+            {
+                throw new ArgumentException("Hasło musi zawierać conajmniej jedną małą literę", nameof(password));
+            }
+            if (!password.Any(char.IsUpper))
+            {
+                throw new ArgumentException("Hasło musi zawierać conajmniej jedną dużą literę", nameof(password));
+            }
+            return true;
+        }
+    }
+}
